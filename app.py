0dc06f909d577e75a9d6abd908e892a4b3bc1a30
Evanderjh <evanderjh@gmail.com>
2021-02-19 19:55:39 -0300

    Calculadora simples em Python

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__, template_folder="./src/views")
+
+
+@app.route("/", methods=["GET", "POST"]) 
+def home():
+    if (request.method == "GET"):
+        return render_template("index.html")
+    else:
+        if (request.form["num1"] != "" and request.form["num2"] != ""):
+
+            num1 = request.form["num1"]
+            num2 = request.form["num2"]
+            opc = request.form["opc"]
+
+            if (opc == "soma"):
+                soma = int(num1) + int(num2)
+                return {
+                    "Resultado": str(soma)
+                }
+
+            elif (opc == "subt"):
+                subt = int(num1) - int(num2)
+                return {
+                    "Resultado": str(subt)
+                }
+            
+            elif (opc == "mult"):
+                mult = int(num1) * int(num2)
+                return {
+                    "Resultado": str(mult)
+                }
+            
+            elif (opc == "divi"):
+                divi = int(num1) / int(num2)
+                return {
+                    "Resultado": str(divi)
+                }
+            
+            else:
+                pont = int(num1) ** int(num2)
+                return {
+                    "Resultado": str(pont)
+                }
+            
+        else:
+            return "Informe um valor válido"
+
+@app.route("/<int:id>") 
+def home_id(id):
+    return str(id+1)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template("error.html")
+
+@app.errorhandler(405) 
+def not_method(error):    
+    return "O verbo não existe"
+
+app.run(port=3333, debug=True) 
